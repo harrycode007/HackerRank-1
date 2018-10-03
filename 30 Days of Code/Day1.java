3ab3e8a0546d09f06a08844247253f2214acdcfc
harrycode007 <40350355+harrycode007@users.noreply.github.com>
2018-10-03 21:07:11 +0530

    Update Day1.java

--- 30 Days of Code/Day1.java
+++ 30 Days of Code/Day1.java
@@ -12,20 +12,18 @@ public class Day1 {
         String s = "HackerRank ";
                 
         Scanner scan = new Scanner(System.in);
-        int a; double b; String c;/* Declare second integer, double, and String variables. */
+        int a; double b; String c;/
 
         a = scan.nextInt();
         b = scan.nextDouble();
         scan.nextLine();
-        c = scan.nextLine();/* Read and save an integer, double, and String to your variables.*/
-        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        c = scan.nextLine();
 
-        System.out.println(i+a);/* Print the sum of both integer variables on a new line. */
-
-        System.out.println(d+b);/* Print the sum of the double variables on a new line. */
+        System.out.println(i+a);
+            
+        System.out.println(d+b);
                 
-        System.out.println(s+c);/* Concatenate and print the String variables on a new line; 
-                the 's' variable above should be printed first. */
+        System.out.println(s+c);
         scan.close();
     }
 }
